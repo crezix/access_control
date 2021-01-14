6c1a8d044824428c5f8971ec15cf51f3339a86f7
crezix <ireshmadhusankha@gmail.com>
2021-01-14 12:48:02 +0530

    commit

--- Basic/detect_mask_video.py
+++ Basic/detect_mask_video.py
@@ -82,7 +82,7 @@ ap.add_argument("-f", "--face", type=str,
                 default="../face_detector",
                 help="path to face detector model directory")
 ap.add_argument("-m", "--model", type=str,
-                default="../model/mask_detector.model",
+                default="./model/mask_detector.model",
                 help="path to trained face mask detector model")
 ap.add_argument("-c", "--confidence", type=float, default=0.5,
                 help="minimum probability to filter weak detections")
@@ -90,8 +90,8 @@ args = vars(ap.parse_args())
 
 # load our serialized face detector model from disk
 print("[INFO] loading face detector model...")
-prototxtPath = "../face_detector/deploy.prototxt"
-weightsPath = "../face_detector/res10_300x300_ssd_iter_140000.caffemodel"
+prototxtPath = "./face_detector/deploy.prototxt"
+weightsPath = "./face_detector/res10_300x300_ssd_iter_140000.caffemodel"
 faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
 
 # load the face mask detector model from disk
