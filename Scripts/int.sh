cc24648c8f88d8aec2733127c407479c072c6743
crezix <ireshmadhusankha@gmail.com>
2021-01-17 17:25:30 +0530

    reqs

---
+++ Scripts/int.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+wget "https://raw.githubusercontent.com/PINTO0309/Tensorflow-bin/master/tensorflow-2.3.0-cp37-none-linux_armv7l_download.sh"
+sudo chmod +x tensorflow-2.3.0-cp37-none-linux_armv7l_download.sh
+./tensorflow-2.3.0-cp37-none-linux_armv7l_download.sh
+sudo pip3 uninstall tensorflow
+sudo -H pip3 install tensorflow-2.3.0-cp37-none-linux_armv7l.whl
