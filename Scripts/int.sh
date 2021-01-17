e3e891c5db5869cf8790c1c1e7f7304d7dc28863
crezix <ireshmadhusankha@gmail.com>
2021-01-17 18:07:15 +0530

    debug

--- Scripts/int.sh
+++ Scripts/int.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-wget "https://raw.githubusercontent.com/PINTO0309/Tensorflow-bin/master/tensorflow-2.3.0-cp37-none-linux_armv7l_download.sh"
-sudo chmod +x tensorflow-2.3.0-cp37-none-linux_armv7l_download.sh
-./tensorflow-2.3.0-cp37-none-linux_armv7l_download.sh
-sudo pip3 uninstall tensorflow
-sudo -H pip3 install tensorflow-2.3.0-cp37-none-linux_armv7l.whl
+#wget "https://raw.githubusercontent.com/PINTO0309/Tensorflow-bin/master/tensorflow-2.3.0-cp37-none-linux_armv7l_download.sh"
+#sudo chmod +x tensorflow-2.3.0-cp37-none-linux_armv7l_download.sh
+#./tensorflow-2.3.0-cp37-none-linux_armv7l_download.sh
+#sudo pip3 uninstall tensorflow
+#sudo -H pip3 install tensorflow-2.3.0-cp37-none-linux_armv7l.whl
+
+pip3 install adafruit-circuitpython-ads1x15
