794b8f3702ce4d443c72992e17eca3123544a90f
crezix <ireshmadhusankha@gmail.com>
2021-01-18 17:31:38 +0530

    set relays

--- Scripts/Outputs.py
+++ Scripts/Outputs.py
@@ -6,8 +6,8 @@ from time import sleep
 rejectIndicator = DigitalOutputDevice(17, initial_value=True)
 successIndicator = DigitalOutputDevice(22, initial_value=True)
 captureLight = DigitalOutputDevice(27, initial_value=True)
-sanitizerLight = LED(25)
-temperatureLight = LED(23)
+sanitizerLight = LED(25, initial_value=True)
+temperatureLight = LED(23, initial_value=True)
 pumpRelay = DigitalOutputDevice(24, initial_value=True)
 
 
@@ -27,18 +27,18 @@ def captureL(state):
 
 def sanitizeL(state):
     if(state):
-        sanitizerLight.on()
-    else:
         sanitizerLight.off()
+    else:
+        sanitizerLight.on()
 
 
 def temperatureL(state):
     if(state == 1):
-        temperatureLight.on()
+        temperatureLight.off()
     elif (state == 2):
         temperatureLight.blink(on_time=0.5, off_time=0.5)
     else:
-        temperatureLight.off()
+        temperatureLight.on()
 
 
 def pump(pumpingTime, webController):
